a8a5acd5e7815fedd5adbe4ab1f49a979a28825d
HU Xiao <hux062303@gmail.com>
2018-11-28 09:41:56 +0100

    final scfs

--- hand_eye_calib/solver/sol_cvx2.m
+++ hand_eye_calib/solver/sol_cvx2.m
@@ -18,7 +18,7 @@ function varargout = sol_cvx2(TA,TB,N)
     
     options = optimoptions('quadprog','Display','off');
     useSCF = 1;
-    useQ = 1;
+    useQ = 0;
     
     if useQ == 1
         Nv = 0;
@@ -79,8 +79,8 @@ function varargout = sol_cvx2(TA,TB,N)
             
         end
     else
-        C = zeros(N*12,12);
-        d = zeros(N*12,1);
+        C = zeros(N*12,13);
+%         d = zeros(N*12,1);
         for i = 1:N
             T1 = TA(i,:,:);T1 = reshape(T1,dim,dim,1);
             T2 = TB(i,:,:);T2 = reshape(T2,dim,dim,1);
@@ -88,29 +88,24 @@ function varargout = sol_cvx2(TA,TB,N)
             Rb = T1(1:3,1:3);tb = T1(1:3,4);
 
             id = (i-1)*12;
-            C(id+1:id+12,:) = [kron(eye(3),Ra)-kron(Rb',eye(3)) zeros(9,3); ...
-                               kron(tb', eye(3)) eye(3)-Ra];
-            d(id+1:id+12) = [zeros(9,1);ta];
+            C(id+1:id+12,:) = [kron(eye(3),Ra)-kron(Rb',eye(3)) zeros(9,3) zeros(9,1); ...
+                               kron(tb', eye(3)) eye(3)-Ra -ta];
         end
-
         As = C'*C;
-        bs = d'*C;
-
         %% vector to optimize
     %     x0 = C\d;
-        x0 = [1 0 0 0 1 0 0 0 1 0 0 0]';
+        x0 = [1 0 0 0 1 0 0 0 1 0 0 0 1]';
         x = x0;
         
         if useSCF == 1
             regualrization = 0;
             AA = 2*blkdiag(As,regualrization);
-            ft = [(-2*bs)';0];
             tic
             for k = 1:1000
+%                 [L,S, Aeq, beq] = conslin(x);
                 [L,S] = conslin(x);
-        %         disp(S);
-                soln = quadprog(AA, ft, L, S,[],[],[],[],[],options);
-                xnew = soln(1:12);
+                soln = quadprog(AA, [], L, S, [], [], [], [], [],options);
+                xnew = soln(1:13);
                 if norm(xnew-x) < 1e-12
                     disp(['converge at step ', num2str(k)]);
                     break;
@@ -119,14 +114,29 @@ function varargout = sol_cvx2(TA,TB,N)
             end
             time = toc;
             disp(['scf :',num2str(time)]);
+
+            tic
+            for k = 1:1000
+                [L,S, Aeq, beq] = conslin3(x);
+%                 [L,S] = conslin(x);
+                soln = quadprog(AA, [], L, S, Aeq, beq, [], [], [],options);
+                xnew = soln(1:13);
+                if norm(xnew-x) < 1e-12
+                    disp(['converge at step ', num2str(k)]);
+                    break;
+                end
+                x = xnew;
+            end
+            time = toc;
+            disp(['scf eq:',num2str(time)]);
+
         else
             AA = 2*As;
-            ft = (-2*bs)';
             tic
             for k = 1:1000
                 [L,S] = conslin2(x);
-                soln = quadprog(AA, ft, [],[],L,S,[],[],[],options);
-                xnew = soln(1:12);
+                soln = quadprog(AA, [], [],[],L,S,[],[],[],options);
+                xnew = soln(1:13);
                 if norm(xnew-x) < 1e-12
                     disp(['converge at step ', num2str(k)]);
                     break;
@@ -161,17 +171,19 @@ function [L,S] = conslin2(x)
     f5 = @(x) (x(4)*x(4)+x(5)*x(5)+x(6)*x(6));
     f6 = @(x) (x(7)*x(7)+x(8)*x(8)+x(9)*x(9));
     
-    df1 = [x(4) x(5) x(6) x(1) x(2) x(3) 0 0 0 0 0 0];
-    df2 = [x(7) x(8) x(9) 0 0 0 x(1) x(2) x(3) 0 0 0];
-    df3 = [0 0 0 x(7) x(8) x(9) x(4) x(5) x(6) 0 0 0];
-    df4 = [2*x(1) 2*x(2) 2*x(3) 0 0 0 0 0 0 0 0 0];
-    df5 = [0 0 0 2*x(4) 2*x(5) 2*x(6) 0 0 0 0 0 0];
-    df6 = [0 0 0 0 0 0 2*x(7) 2*x(8) 2*x(9) 0 0 0];
+    df1 = [x(4) x(5) x(6) x(1) x(2) x(3) 0 0 0 0 0 0 0];
+    df2 = [x(7) x(8) x(9) 0 0 0 x(1) x(2) x(3) 0 0 0 0];
+    df3 = [0 0 0 x(7) x(8) x(9) x(4) x(5) x(6) 0 0 0 0];
+    df4 = [2*x(1) 2*x(2) 2*x(3) 0 0 0 0 0 0 0 0 0 0];
+    df5 = [0 0 0 2*x(4) 2*x(5) 2*x(6) 0 0 0 0 0 0 0];
+    df6 = [0 0 0 0 0 0 2*x(7) 2*x(8) 2*x(9) 0 0 0 0];
     
     h = [0 0 0 1 1 1]';
     L = [df1;df2;df3;df4;df5;df6];
     b1 = [f1(x)-df1*x;f2(x)-df2*x;f3(x)-df3*x;f4(x)-df4*x;f5(x)-df5*x;f6(x)-df6*x];
     S = h-b1;
+    L = [L;zeros(1,12) 1];
+    S = [S;1];
 end
 
 function [L,S] = conslin(x)
@@ -181,17 +193,48 @@ function [L,S] = conslin(x)
     f4 = @(x) (x(1)*x(1)+x(2)*x(2)+x(3)*x(3));
     f5 = @(x) (x(4)*x(4)+x(5)*x(5)+x(6)*x(6));
     f6 = @(x) (x(7)*x(7)+x(8)*x(8)+x(9)*x(9));
-    
-    df1 = [x(4) x(5) x(6) x(1) x(2) x(3) 0 0 0 0 0 0];
-    df2 = [x(7) x(8) x(9) 0 0 0 x(1) x(2) x(3) 0 0 0];
-    df3 = [0 0 0 x(7) x(8) x(9) x(4) x(5) x(6) 0 0 0];
-    df4 = [2*x(1) 2*x(2) 2*x(3) 0 0 0 0 0 0 0 0 0];
-    df5 = [0 0 0 2*x(4) 2*x(5) 2*x(6) 0 0 0 0 0 0];
-    df6 = [0 0 0 0 0 0 2*x(7) 2*x(8) 2*x(9) 0 0 0];
-    
+    f7 = @(x) (x(13));
+
+    df1 = [x(4) x(5) x(6) x(1) x(2) x(3) 0 0 0 0 0 0 0];
+    df2 = [x(7) x(8) x(9) 0 0 0 x(1) x(2) x(3) 0 0 0 0];
+    df3 = [0 0 0 x(7) x(8) x(9) x(4) x(5) x(6) 0 0 0 0];
+    df4 = [2*x(1) 2*x(2) 2*x(3) 0 0 0 0 0 0 0 0 0 0];
+    df5 = [0 0 0 2*x(4) 2*x(5) 2*x(6) 0 0 0 0 0 0 0];
+    df6 = [0 0 0 0 0 0 2*x(7) 2*x(8) 2*x(9) 0 0 0 0];
+    df7 = [0 0 0 0 0 0 0 0 0 0 0 0 1];
+
     h = [0 0 0 1 1 1]';
     a1 = [df1;df2;df3;df4;df5;df6];
     L = [[-a1 -h];[-a1 h]];
     b1 = [f1(x)-df1*x;f2(x)-df2*x;f3(x)-df3*x;f4(x)-df4*x;f5(x)-df5*x;f6(x)-df6*x];
     S = [b1;b1];
+%     A = [df7 0];
+%     b = 1;
+    L = [L;[zeros(1,12) 1 0];[zeros(1,12) -1 0]];
+    S = [S;1;-1];
+end
+
+function [L,S,A,b] = conslin3(x)
+    f1 = @(x) (x(1)*x(4)+x(2)*x(5)+x(3)*x(6));
+    f2 = @(x) (x(1)*x(7)+x(2)*x(8)+x(3)*x(9));
+    f3 = @(x) (x(4)*x(7)+x(5)*x(8)+x(6)*x(9));
+    f4 = @(x) (x(1)*x(1)+x(2)*x(2)+x(3)*x(3));
+    f5 = @(x) (x(4)*x(4)+x(5)*x(5)+x(6)*x(6));
+    f6 = @(x) (x(7)*x(7)+x(8)*x(8)+x(9)*x(9));
+
+    df1 = [x(4) x(5) x(6) x(1) x(2) x(3) 0 0 0 0 0 0 0];
+    df2 = [x(7) x(8) x(9) 0 0 0 x(1) x(2) x(3) 0 0 0 0];
+    df3 = [0 0 0 x(7) x(8) x(9) x(4) x(5) x(6) 0 0 0 0];
+    df4 = [2*x(1) 2*x(2) 2*x(3) 0 0 0 0 0 0 0 0 0 0];
+    df5 = [0 0 0 2*x(4) 2*x(5) 2*x(6) 0 0 0 0 0 0 0];
+    df6 = [0 0 0 0 0 0 2*x(7) 2*x(8) 2*x(9) 0 0 0 0];
+    df7 = [0 0 0 0 0 0 0 0 0 0 0 0 1];
+
+    h = [0 0 0 1 1 1]';
+    a1 = [df1;df2;df3;df4;df5;df6];
+    L = [[-a1 -h];[-a1 h]];
+    b1 = [f1(x)-df1*x;f2(x)-df2*x;f3(x)-df3*x;f4(x)-df4*x;f5(x)-df5*x;f6(x)-df6*x];
+    S = [b1;b1];
+    A = [df7 0];
+    b = 1;
 end
