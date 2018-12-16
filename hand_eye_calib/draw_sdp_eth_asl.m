dd9c7881b15241fd72b0ae60eb819f056c20c72e
DTU Space drone <xiahaa@space.dtu.dk>
2018-12-16 18:13:19 +0100

    draw eth asl

--- hand_eye_calib/draw_sdp_eth_asl.m
+++ hand_eye_calib/draw_sdp_eth_asl.m
@@ -8,7 +8,7 @@ addpath('./solver/atadq');
 addpath('../beautiful_plot/aboxplot');
 
 for id = 1:4
-    clear dat;
+%     clear dat;
     dat(id) = load(strcat('./data/sdp/','prime_', num2str(id),'_res','.mat'));
     numCls = numel(dat(id).tsol);
     convSols = dat(id).convSols;
@@ -27,15 +27,15 @@ cmap = lines(numCls);
 cmapalpha = [cmap 0.2*ones(size(cmap,1),1)];
     
 %% 1
-font_size = 15;
+font_size = 12;
 line_width = 2.5;
 subplot_margin = 0.12;
-subplot_spacing = 0.1;
+subplot_spacing = 0.03;
 fig = figure();
 % grid on;
 set(fig,'defaulttextinterpreter','latex');
-subplot(4,2,1);
-% subaxis(4,2,1, 'Margin', subplot_margin, 'Spacing', subplot_spacing);
+% subplot(1,2,1);
+subaxis(1,2,1, 'Margin', subplot_margin, 'Spacing', subplot_spacing);
 box_labels = categorical(convSols);
 box_labels = reordercats(box_labels,convSols);
 boxplot((dat(1).errcs1)',box_labels);grid on;
@@ -46,9 +46,10 @@ for jj=1:length(h)
 end
 decorateBox();
 ylabel('$RMSE\ Rotation$','Interpreter','latex','FontSize',font_size);
+xticklabel_rotate([],45,[],'Fontsize',font_size);
 
-subplot(4,2,2);
-% subaxis(4,2,2, 'Margin', subplot_margin, 'Spacing', subplot_spacing);
+% subplot(1,2,2);
+subaxis(1,2,2, 'Margin', subplot_margin, 'Spacing', subplot_spacing);
 box_labels = categorical(convSols);
 box_labels = reordercats(box_labels,convSols);
 boxplot((dat(1).errcs2)',box_labels);grid on;
@@ -59,13 +60,16 @@ for jj=1:length(h)
 end
 decorateBox();
 ylabel('$RMSE\ Translation$','Interpreter','latex','FontSize',font_size);
-
+xticklabel_rotate([],45,[],'Fontsize',font_size);
+[~,h1]=suplabel('$primesense\_1$' ,'t'); 
+set(h1,'Interpreter','latex','FontSize',font_size);
 % ha = axes('Position',[0 0 1 1],'Xlim',[0 1],'Ylim',[0  1],'Box','off','Visible','off','Units','normalized', 'clipping' , 'off');
 % text(0.5, 0.98,'Title')
 
 %% 2
-subplot(4,2,3);
-% subaxis(4,2,3, 'Margin', subplot_margin, 'Spacing', subplot_spacing);
+fig = figure();
+% subplot(1,2,1);
+subaxis(1,2,1, 'Margin', subplot_margin, 'Spacing', subplot_spacing);
 box_labels = categorical(convSols);
 box_labels = reordercats(box_labels,convSols);
 boxplot(dat(2).errcs1',box_labels);grid on;
@@ -76,9 +80,10 @@ for jj=1:length(h)
 end
 decorateBox();
 ylabel('$RMSE\ Rotation$','Interpreter','latex','FontSize',font_size);
+xticklabel_rotate([],45,[],'Fontsize',font_size);
 
-subplot(4,2,4);
-% subaxis(4,2,4, 'Margin', subplot_margin, 'Spacing', subplot_spacing);
+% subplot(1,2,2);
+subaxis(1,2,2, 'Margin', subplot_margin, 'Spacing', subplot_spacing);
 box_labels = categorical(convSols);
 box_labels = reordercats(box_labels,convSols);
 boxplot(dat(2).errcs2',box_labels);grid on;
@@ -89,9 +94,13 @@ for jj=1:length(h)
 end
 decorateBox();
 ylabel('$RMSE\ Translation$','Interpreter','latex','FontSize',font_size);
+xticklabel_rotate([],45,[],'Fontsize',font_size);
+[~,h1]=suplabel('$primesense\_2$' ,'t'); 
+set(h1,'Interpreter','latex','FontSize',font_size);
 
 %% 3
-% subplot(4,2,3);
+fig = figure();
+% subplot(1,2,1);
 subaxis(1,2,1, 'Margin', subplot_margin, 'Spacing', subplot_spacing);
 box_labels = categorical(convSols);
 box_labels = reordercats(box_labels,convSols);
@@ -103,7 +112,10 @@ for jj=1:length(h)
 end
 decorateBox();
 ylabel('$RMSE\ Rotation$','Interpreter','latex','FontSize',font_size);
-subaxis(4,2,4, 'Margin', subplot_margin, 'Spacing', subplot_spacing);
+xticklabel_rotate([],45,[],'Fontsize',font_size);
+
+% subplot(1,2,2);
+subaxis(1,2,2, 'Margin', subplot_margin, 'Spacing', subplot_spacing);
 box_labels = categorical(convSols);
 box_labels = reordercats(box_labels,convSols);
 boxplot(dat(3).errcs2',box_labels);grid on;
@@ -113,10 +125,13 @@ for jj=1:length(h)
    patch(get(h(jj),'XData'),get(h(jj),'YData'),cmapalpha(jj,1:3),'FaceAlpha',cmapalpha(jj,4));
 end
 decorateBox();
-ylabel('$RMSE\ Translation$','Interpreter','latex','FontSize',font_size);
+ylabel('$RMSE\ Translation$','Interpreter','latex','FontSize',font_size);xticklabel_rotate([],45,[],'Fontsize',font_size);
+[~,h1]=suplabel('$robotarm$' ,'t'); 
+set(h1,'Interpreter','latex','FontSize',font_size);
 
 %% 4
-subplot(4,1,4);
+fig = figure();
+% subplot(1,2,1);
 subaxis(1,2,1, 'Margin', subplot_margin, 'Spacing', subplot_spacing);
 box_labels = categorical(convSols);
 box_labels = reordercats(box_labels,convSols);
@@ -128,6 +143,9 @@ for jj=1:length(h)
 end
 decorateBox();
 ylabel('$RMSE\ Rotation$','Interpreter','latex','FontSize',font_size);
+xticklabel_rotate([],45,[],'Fontsize',font_size);
+
+% subplot(1,2,2);
 subaxis(1,2,2, 'Margin', subplot_margin, 'Spacing', subplot_spacing);
 box_labels = categorical(convSols);
 box_labels = reordercats(box_labels,convSols);
@@ -138,7 +156,9 @@ for jj=1:length(h)
    patch(get(h(jj),'XData'),get(h(jj),'YData'),cmapalpha(jj,1:3),'FaceAlpha',cmapalpha(jj,4));
 end
 decorateBox();
-ylabel('$RMSE\ Translation$','Interpreter','latex','FontSize',font_size);
+ylabel('$RMSE\ Translation$','Interpreter','latex','FontSize',font_size);xticklabel_rotate([],45,[],'Fontsize',font_size);
+[~,h1]=suplabel('$robotarm\_sim$' ,'t'); 
+set(h1,'Interpreter','latex','FontSize',font_size);
 
 function decorateBox()
     hlw = findall(gca,'tag','Lower Whisker');
