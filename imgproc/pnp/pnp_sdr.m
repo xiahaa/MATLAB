962ba75b17dbad523e705704b9e3c4504d8c6e9b
DTU Space drone <xiahaa@space.dtu.dk>
2019-01-22 14:08:36 +0100

    sdr solution for pnp
    work but very slow and not quite stable.

--- imgproc/pnp/pnp_sdr.m
+++ imgproc/pnp/pnp_sdr.m
@@ -7,57 +7,76 @@ function [R,t] = pnp_sdr(P, q, K, R, t)
         q = [q;ones(1,n)];
     end
     qn = K\q;
-    vi = qn;
-%     vi = qn ./ sqrt(qn(1,:).^2+qn(2,:).^2+qn(3,:).^2);
+%     vi = qn;
+    vi = qn ./ sqrt(qn(1,:).^2+qn(2,:).^2+qn(3,:).^2);
     
     %% formulate
-    nopt = 9 + 3;% rotation + translation + slack
-    Q = zeros(nopt,nopt);
+    nopt = 9 + 1;% rotation + translation + slack
+    Qi = zeros(3,3,n);
+    QiCci = zeros(3,nopt,n);
+    F1 = zeros(3,3);
+    F2 = zeros(3,nopt);
     for i = 1:n
         Vi = vi(:,i)*vi(:,i)'/(vi(:,i)'*vi(:,i));
-        IVVI = (eye(3)-Vi)'*(eye(3)-Vi);
-        A = [P(1,i) 0 0 P(2,i) 0 0 P(3,i) 0 0 1 0 0; ...
-             0 P(1,i) 0 0 P(2,i) 0 0 P(3,i) 0 0 1 0; ...
-             0 0 P(1,i) 0 0 P(2,i) 0 0 P(3,i) 0 0 1];
-        Q = Q + A'*IVVI*A;
+        Qi(:,:,i) = (eye(3)-Vi)'*(eye(3)-Vi);
+        F1 = F1 + Qi(:,:,i);
+        
+        Cd = [P(:,i)' 0 0 0 0 0 0 -qn(1,i); ...
+              0 0 0 P(:,i)' 0 0 0 -qn(2,i); ...
+              0 0 0 0 0 0 P(:,i)' -qn(3,i)];
+        QiCci(:,:,i) = Qi(:,:,i) * Cd;
+        F2 = F2 + QiCci(:,:,i);
     end
-    Q = (Q+Q').*0.5;% force symmetric
-%     xxx = [R(1,:)';R(2,:)';R(3,:)';t];
+    F1inv = inv(F1);
+    F3 = -F1inv * F2;
+    M = zeros(nopt, nopt);
+    for i = 1:n
+        Cd = [P(:,i)' 0 0 0 0 0 0 -qn(1,i); ...
+              0 0 0 P(:,i)' 0 0 0 -qn(2,i); ...
+              0 0 0 0 0 0 P(:,i)' -qn(3,i)];
+        M = M + (Cd + F3)'*Qi(:,:,i)*(Cd + F3);
+    end
+    M = (M+M').*0.5;% force symmetric
+    xxx = [R(1,:)';R(2,:)';R(3,:)';1];
     
-    [R, t, time] = solve_SDP_kron(nopt, Q, P(:,1));
+    ext = [0 0 0 0 0 0 P(:,i)' 0] + F3(3,:);
+    [R, x, time] = solve_SDP_kron(nopt, M, ext);
+    t = F3*x;
 end
 
-function [R, t, time] = solve_SDP_kron(nopt, Q, varargin)
+function [R, x, time] = solve_SDP_kron(nopt, Q, varargin)
     AA = Q;
     n = nopt;
     
     K = 3*n;
     
     A1 = zeros(n,n);
-    A1(1,4) = 1;A1(2,5) = 1;A1(3,6) = 1;
+    A1(1,2) = 1;A1(4,5) = 1;A1(7,8) = 1;
     A2 = zeros(n,n);
-    A2(1,7) = 1;A2(2,8) = 1;A2(3,9) = 1;
+    A2(1,3) = 1;A2(4,6) = 1;A2(7,9) = 1;
     A3 = zeros(n,n);
-    A2(4,7) = 1;A2(5,8) = 1;A2(6,9) = 1;
+    A2(2,3) = 1;A2(5,6) = 1;A2(8,9) = 1;
     A4 = zeros(n,n);
-    A4(1,1) = 1;A4(2,2) = 1;A4(3,3) = 1;
+    A4(1,1) = 1;A4(4,4) = 1;A4(7,7) = 1;
     A5 = zeros(n,n);
-    A5(4,4) = 1;A5(5,5) = 1;A5(6,6) = 1;
+    A5(2,2) = 1;A5(5,5) = 1;A5(8,8) = 1;
     A6 = zeros(n,n);
-    A6(7,7) = 1;A6(8,8) = 1;A6(9,9) = 1;
-    A7 = zeros(n,n);
-    A7(n,n) = 1;
+    A6(3,3) = 1;A6(6,6) = 1;A6(9,9) = 1;
+%     A7 = zeros(n,n);
+%     A7(n,n) = 1;
 %     format long;
     ext = 0;
     if nargin == 3
         va = varargin{1};
         Aext = zeros(n,n); 
         %% r31*P(1)+r32*P(2)+r33*P(3)+t(3) > 0 chirality constraint
-        Aext(7,13) = va(1);Aext(8,13) = va(2);Aext(9,13) = va(3);Aext(12,13) = -1;
+%         Aext(7,end) = va(1);Aext(8,end) = va(2);Aext(9,end) = va(3);Aext(12,13) = -1;
+        Aext(:,end) = va';
+        ext = 1;
     end
     
     tic
-    cvx_begin 
+    cvx_begin quiet
         variable X(n, n) symmetric
         minimize ( trace(AA*X) )
         subject to
@@ -69,9 +88,9 @@ function [R, t, time] = solve_SDP_kron(nopt, Q, varargin)
             trace(A4*X) == 1;
             trace(A5*X) == 1;
             trace(A6*X) == 1;
-%             X(n,n) == 1;
+%             trace(A7*X) == 1;
             if ext == 1
-%                 trace(Aext*X) > 0;
+                trace(Aext*X) > 0;
             end
     cvx_end
     lb = cvx_optval;
@@ -97,11 +116,11 @@ function [R, t, time] = solve_SDP_kron(nopt, Q, varargin)
     time = toc;
     x = xstar;
     disp(['SDP + randomization :',num2str(time)]);
-    R12 = [x(1) x(4) x(7); ...
-           x(2) x(5) x(8); ...
-           x(3) x(6) x(9)];
-    t12 = x(10:12);
+    R12 = [x(1) x(2) x(3); ...
+           x(4) x(5) x(6); ...
+           x(7) x(8) x(9)];
+%     t12 = x(10:12);
 %     R12 = R12*inv(sqrtm(R12'*R12));
     R = R12;
-    t = t12;
+%     t = t12;
 end
