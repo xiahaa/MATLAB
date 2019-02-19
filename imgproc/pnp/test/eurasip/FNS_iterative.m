cbaf1da94163448d331870475977522430627089
DTU Space drone <xiahaa@space.dtu.dk>
2019-02-19 19:41:44 +0100

    weighted solution

--- imgproc/pnp/test/eurasip/FNS_iterative.m
+++ imgproc/pnp/test/eurasip/FNS_iterative.m
@@ -1,22 +1,27 @@
-function M = FNS_iterative(SO3s,M_1st)
+function M = FNS_iterative(SO3s,M_1st,varargin)
 % My implementation of iterative optimizing the mean using manfiold
 % optimization technique. It works directly with the definition of the mean
 % and applies optimization directly on SO3.
     M = M_1st;
     iter = 1;
-    maxIter = 50;
+    maxIter = 20;
+    if nargin == 2
+        weight = ones(1,size(SO3s,3))./size(SO3s,3);
+    else
+        weight = varargin{1};
+    end
     while iter < maxIter
         Minv = (M)';
         LHS = zeros(3,3);
         RHS = zeros(3,1);
         for k=1:size(SO3s,3)
             v1 = rot2vec(Minv*SO3s(:,:,k));
-            RHS = RHS + v1;
-            LHS = LHS + vec2jacInv(v1);
+            RHS = RHS + weight(k).*v1;
+            LHS = LHS + weight(k).*vec2jacInv(v1);
         end
         xhat = LHS\RHS;
         M = M*vec2rot(xhat);
-        if norm(xhat) < 1e-3
+        if norm(xhat) < 1e-6
             break;
         end
         iter = iter + 1;
--- imgproc/pnp/test/eurasip/mean_1st_order.m
+++ imgproc/pnp/test/eurasip/mean_1st_order.m
@@ -1,14 +1,21 @@
-function M = mean_1st_order(SO3s)
+function M = mean_1st_order(SO3s, varargin)
 % first order mean.
     addpath C:\Users\xiahaa\Documents\MATLAB\MatrixLieGroup\barfoot_tro14\
     M = zeros(3,3);
-    for i = 1:size(SO3s,3)
-        M = M + SO3s(:,:,i);
+    
+    if nargin == 1
+        weight = ones(1,size(SO3s,3))./size(SO3s,3);
+    else
+        weight = varargin{1};
     end
-    M = M ./ size(SO3s,3);
+    
+    for i = 1:size(SO3s,3)
+        M = M + weight(i).*SO3s(:,:,i);
+    end
+%     M = M ./ size(SO3s,3);
+
     %% orthogonalization
-%     [U, ~, V] = svd(M); 
-%     M = U*V';
-%     if det(M)<0, M = U*diag([1 1 -1])*V'; end 
-     M = orthog(M);
+    [U, ~, V] = svd(M); 
+    M = U*V';
+    if det(M)<0, M = U*diag([1 1 -1])*V'; end 
 end
