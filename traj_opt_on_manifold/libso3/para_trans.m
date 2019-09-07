e575f0774893c201c3539fcd15faacb4604ee1f7
HU Xiao <hux062303@gmail.com>
2019-09-07 22:56:18 +0200

    delete useless code

--- traj_opt_on_manifold/libso3/para_trans.m
+++ traj_opt_on_manifold/libso3/para_trans.m
@@ -6,3 +6,14 @@ function vpt = para_trans(R1,R2,v)
     % fast version
     vpt = expSO3(logSO3(R2'*R1)/2)*v;
 end
+
+
+%function vpt = para_trans(R1,R2,v)
+%    A = logSO3(R1'*R2);
+%    Rpt = expSO3(A/2);
+%    if size(v,2) == 1
+%        vpt = invhat(R2'*R1*Rpt*hat(v)*Rpt);
+%    else
+%        vpt = invhat(R2'*R1*Rpt*(R1'*v)*Rpt);
+%    end
+%end
