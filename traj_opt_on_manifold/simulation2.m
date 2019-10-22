1780c255400db7d08c7239b0d3bbfb8ba74c6571
HU Xiao <hux062303@gmail.com>
2019-10-22 23:08:15 +0200

    test

--- traj_opt_on_manifold/simulation2.m
+++ traj_opt_on_manifold/simulation2.m
@@ -14,8 +14,8 @@ data = load('controlpoints.mat');
 
 %% Define parameters of the discrete regression curve
 % The curve has Nd points on SO(n)
-% Nd = 10:10:100;
-Nd = 50;
+Nd = 10:10:100;
+% Nd = 50;
 
 for i = 1:length(Nd)
     [cost1, cost2, cost3] = regression_comparison(data, Nd(i));
@@ -101,33 +101,37 @@ function [cost1, cost2, cost3] = regression_comparison(data, Nd)
     newcosts = zeros(1,maxiter);
     Rreg2 = Rreg;
     tic
-    while iter <= maxiter
-        % sequential update
-        newcost = 0;
-%         ids = randperm(N2,N2);
-        for j = 1:N2
-            id = j;%ids(j);
-            xi = data_term_error(Rdata,Rreg2,indices,id);
-            v = numerical_diff_v(Rreg2,id);
-            dxi = seq_sol(xi, v, indices, tau, lambda, miu, N2, id);
-            % 
-            if norm(dxi) > tr
-                dxi = dxi ./ norm(dxi) .* tr;
-            end
-            dxis(:,id)=dxi;
-            Rreg2(:,id*3-2:id*3) = Rreg2(:,id*3-2:id*3) * expSO3(dxi);
-        end
-        
-        xi = data_term_error(Rdata,Rreg2,indices);
-        v = numerical_diff_v(Rreg2);
-        newcost = cost(xi,v,tau,lambda,miu);
-        newcosts(iter) = newcost;
-        if abs(newcost - oldcost) < tol1
-            break;
-        end
-        oldcost = newcost;
-        iter = iter + 1;
-    end
+%     while iter <= maxiter
+%         % sequential update
+%         newcost = 0;
+% %         ids = randperm(N2,N2);
+%         for j = 1:N2
+%             id = j;%ids(j);
+%             xi = data_term_error(Rdata,Rreg2,indices,id);
+%             v = numerical_diff_v(Rreg2,id);
+%             dxi = seq_sol(xi, v, indices, tau, lambda, miu, N2, id);
+%             % 
+%             if norm(dxi) > tr
+%                 dxi = dxi ./ norm(dxi) .* tr;
+%             end
+%             dxis(:,id)=dxi;
+%             Rreg2(:,id*3-2:id*3) = Rreg2(:,id*3-2:id*3) * expSO3(dxi);
+%         end
+%         
+%         xi = data_term_error(Rdata,Rreg2,indices);
+%         v = numerical_diff_v(Rreg2);
+%         newcost = cost(xi,v,tau,lambda,miu);
+%         newcosts(iter) = newcost;
+%         if abs(newcost - oldcost) < tol1
+%             break;
+%         end
+%         oldcost = newcost;
+%         iter = iter + 1;
+%     end
+    Rreg2 = coarse_to_fine_seq_sol(Rdata, Rreg, indices, tau, lambda, miu, Nd);
+    xi = data_term_error(Rdata,Rreg2,indices);
+    v = numerical_diff_v(Rreg2);
+    newcosts(1) = cost(xi,v,tau,lambda,miu);
     cost2.time = toc;
     cost2.cost = newcosts(end);
     
@@ -164,6 +168,69 @@ function [cost1, cost2, cost3] = regression_comparison(data, Nd)
     ylim([0,100]);
 end
 
+function Rreg = coarse_to_fine_seq_sol(Rdata, Rreg, indices, tau, lambda, miu, N)
+    % start from 30
+    Ns = 10;
+    N0 = Ns;
+    Rreg = reshape(Rreg,3,3,[]);
+    options = optimoptions('quadprog','MaxIterations',100,'OptimalityTolerance',1e-5,'StepTolerance',1e-5,'Display','off');
+    while N0 <= N
+        Ncur = round(linspace(1,N,N0));
+        for i = 1:length(indices)
+            if isempty(find(Ncur == indices(i),1))
+                Ncur = [Ncur indices(i)];
+            end
+        end
+        Ncur = sort(Ncur,'ascend');
+        indicescur = indices;
+        for i = 1:length(indices)
+            indicescur(i) = find(Ncur==indices(i),1);
+        end
+        
+        iter = 1;
+        maxiter = 50;
+        oldcost = inf;
+        tol1 = 1e-6;
+%         tr = 1;
+        N2 = length(Ncur);
+        Rregcur = Rreg(:,:,Ncur);
+        Rregcur = reshape(Rregcur,3,[]);
+        while iter < maxiter
+            newcost = -1e6;
+            for j = 1:N2
+                id = j;
+                xi = data_term_error(Rdata,Rregcur,indicescur,id);
+                v = numerical_diff_v(Rregcur,id);
+                dxi = seq_sol(xi, v, indicescur, tau, lambda, miu, N2, id);
+%                 if norm(dxi) > tr
+%                     dxi = dxi ./ norm(dxi) .* tr;
+%                 end
+                Rregcur(:,id*3-2:id*3) = Rregcur(:,id*3-2:id*3) * expSO3(dxi);
+                if norm(dxi) > newcost
+                    newcost = norm(dxi);
+                end
+            end
+            if abs(newcost - oldcost) < tol1
+                break;
+            else
+                oldcost = newcost;
+            end
+            iter = iter + 1;
+        end
+        Rregcur = reshape(Rregcur,3,3,[]);
+%         [speed0, acc0] = compute_profiles_fast(tau,Rregcur);
+%         figure(1);
+%         plot(1:N2,speed0,1:N2,acc0);
+        Rreg(:,:,Ncur) = Rregcur;
+        if N0 >= N
+            break;
+        else
+            N0 = min(N0+Ns,N);
+        end
+        
+    end
+    Rreg = reshape(Rreg,3,[]);
+end
     
     
 %   
