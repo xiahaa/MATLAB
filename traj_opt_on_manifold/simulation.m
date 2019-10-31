3e8ac005f9bcb65d4ba3c6872b9bfb3a55455ca9
DTU Space drone <xiahaa@space.dtu.dk>
2019-10-31 15:09:31 +0100

    new figures and new simulation programme

--- traj_opt_on_manifold/simulation.m
+++ traj_opt_on_manifold/simulation.m
@@ -12,7 +12,7 @@ for i = 1:length(t)
     Rnoisy(:,:,i) = angle2dcm(yaw(i)+ 0.1 * randn(1), pitch(i)+ 0.1 * randn(1), roll(i)+ 0.1 * randn(1));
 end
 tic
-Rreg = regression_so3(Rnoisy);
+Rreg = regression_so3(Rnoisy,Rreal);
 toc;
 
 Rreg1 = boumel(Rnoisy);
@@ -34,6 +34,7 @@ for i = 2:length(t)
     anglereg2(i) = norm(so3) * 180 / pi;
 end
 colormap=jet;
+ts = linspace(0,1,length(anglereal));
 figure
 plot(anglereal,'LineWidth',1.5);hold on;grid on;
 plot(anglenoisy,'LineWidth',1.5);
@@ -46,7 +47,7 @@ legend({'Ground Truth','Input','Our','Bou','Jia'},'Location','northwest');
 
 
 
-function X1 = regression_so3(Rreg)
+function X1 = regression_so3(Rreg,Rreal)
     if ismac
         addpath './utils/'
         addpath './libso3'
@@ -79,7 +80,7 @@ function X1 = regression_so3(Rreg)
     % Weight of the velocity regularization term (nonnegative). The larger it
     % is, the more velocity along the discrete curve is penalized. A large
     % value usually results in a shorter curve.
-    lambda = 0.1;
+    lambda = 0;
 
     % Weight of the acceleration regularization term (nonnegative). The larger
     % it is, the more acceleration along the discrete curve is penalized. A
@@ -157,113 +158,119 @@ function X1 = regression_so3(Rreg)
     [Rreg,newcosts] = non_optimization_on_so3(Rdata, Rreg, miu, lambda, indices, tau, 4, 0.25);
     X1 = reshape(Rreg,3,3,[]);
     
-%     figure(7);
-%     plot(newcosts,'r-','LineWidth',1.5); grid on;
-%     xlabel('Iteration');
-%     ylabel('Objective Function Value');
-%     end
-%     
-%     showSO3(Rdata,Rreg);
-%     
-%     for i = 1:N2
-%         X1(:,:,i) = Rreg(:,i*3-2:i*3);
-%     end
-%     
-%     [speed0, acc0] = compute_profiles(problem, X0);
-%     [speed1, acc1] = compute_profiles(problem, X1);
-% 
-%     % Passage time of each point on the discrete curves.
-%     time = problem.delta_tau*( 0 : (problem.Nd-1) );
-% 
-%     figure(5);
-%     subplot(2, 1, 1);
-%     plot(1:N2,speed0,1:N2,speed1);
-% %     plot(time, speed0, time, speed1);
-%     title('Speed of initial curve and optimized curve');
-%     xlabel('Frame');
-%     ylabel('Speed');
-%     legend('Initial curve', 'Optimized curve', 'Location', 'NorthWest');
-%     pbaspect([1.6, 1, 1]);
-% 
-%     subplot(2, 1, 2);
-%     plot(1:N2,acc0,1:N2,acc1);
-% %     plot(time, acc0, time, acc1);
-%     title('Acceleration of initial curve and optimized curve');
-%     xlabel('Frame');
-%     ylabel('Acceleration');
-%     legend('Initial curve', 'Optimized curve', 'Location', 'NorthWest');
-%     pbaspect([1.6, 1, 1]);
-%     
-%     % Extend final sample to delay end of animation
-%     Rdata = reshape(Rdata,3,3,[]);
-%     Rreg = reshape(Rreg,3,3,[]);
-%     
-%     for i = 1:size(Rreg,3)
-%         quat(i,:) = rot2quat(Rreg(:,:,i))';
-%         pos(i,:) = (i-1).*[0.01,0.01,0.01];
-%     end
-%     
-%     for i = 1:size(Rdata,3)
-%         quatd(i,:) = rot2quat(Rdata(:,:,i))';
-%     end
-%     posd = pos(indices,:);
-% 
-%     
-%     posPlot = pos;
-%     quatPlot = quat;
-% 
-%     
-%     extraTime = 1;
-%     samplePeriod = 1/100;
-%     onesVector = ones(extraTime*(1/samplePeriod), 1);
-%     posPlot = [posPlot; [posPlot(end, 1)*onesVector, posPlot(end, 2)*onesVector, posPlot(end, 3)*onesVector]];
-%     quatPlot = [quatPlot; [quatPlot(end, 1)*onesVector, quatPlot(end, 2)*onesVector, quatPlot(end, 3)*onesVector, quatPlot(end, 4)*onesVector]];
-% 
-%     % Create 6 DOF animation
-%     SamplePlotFreq = 2;
-%                 
-%     for i = 1:size(X0,3)
-%         quat0(i,:) = rot2quat(X0(:,:,i))';
-%     end
-%     ts = linspace(0,1,size(Rreg,3));
-%     t0 = ts;
-%     
-%     figure
-%     subplot(2,2,1);
-%     plot(1:N2, quat(:,1),'r-','LineWidth',2);
-%     hold on;grid on;
-%     line=plot(1:N2, quat0(:,1),'go','LineWidth',1);
-%     alpha(line,0.1);
-%     legend({'Regression','Input'},'FontName','Arial','FontSize',15);
-% %     title('q0','FontName','Arial','FontSize',15);
-%     xlabel('Frame');
-%     ylabel('q0');
-%     
-%     subplot(2,2,2);
-%     plot(1:N2, quat(:,2),'r-','LineWidth',2);
-%     hold on;grid on;
-%     plot(1:N2, quat0(:,2),'go','LineWidth',1);
-% %     title('q1','FontName','Arial','FontSize',15);
-%     xlabel('Frame');
-%     ylabel('q1');
-%     
-%     subplot(2,2,3);
-%     plot(1:N2, quat(:,3),'r-','LineWidth',2);
-%     hold on;grid on;
-%     plot(1:N2, quat0(:,3),'go','LineWidth',1);
-%     title('q2','FontName','Arial','FontSize',15);
-%     xlabel('Frame');
-%     ylabel('q2');
-%     
-%     subplot(2,2,4);
-%     plot(1:N2, quat(:,4),'r-','LineWidth',2);
-%     hold on;grid on;
-%     plot(1:N2, quat0(:,4),'go','LineWidth',1);
-%     title('q3','FontName','Arial','FontSize',15);
-%     xlabel('Frame');
-%     ylabel('q3');
-%                 
+    figure(7);
+    plot(newcosts,'r-','LineWidth',1.5); grid on;
+    xlabel('Iteration');
+    ylabel('Objective Function Value');
     end
+    
+    showSO3(Rdata,Rreg);
+    
+    for i = 1:N2
+        X1(:,:,i) = Rreg(:,i*3-2:i*3);
+    end
+    
+    [speed0, acc0] = compute_profiles(problem, X0);
+    [speed1, acc1] = compute_profiles(problem, X1);
+
+    % Passage time of each point on the discrete curves.
+    time = problem.delta_tau*( 0 : (problem.Nd-1) );
+
+    figure(5);
+    subplot(2, 1, 1);
+    plot(1:N2,speed0,1:N2,speed1);
+%     plot(time, speed0, time, speed1);
+    title('Speed of initial curve and optimized curve');
+    xlabel('Frame');
+    ylabel('Speed');
+    legend('Initial curve', 'Optimized curve', 'Location', 'NorthWest');
+    pbaspect([1.6, 1, 1]);
+
+    subplot(2, 1, 2);
+    plot(1:N2,acc0,1:N2,acc1);
+%     plot(time, acc0, time, acc1);
+    title('Acceleration of initial curve and optimized curve');
+    xlabel('Frame');
+    ylabel('Acceleration');
+    legend('Initial curve', 'Optimized curve', 'Location', 'NorthWest');
+    pbaspect([1.6, 1, 1]);
+    
+    % Extend final sample to delay end of animation
+    Rdata = reshape(Rdata,3,3,[]);
+    Rreg = reshape(Rreg,3,3,[]);
+    for i = 1:size(Rreg,3)
+        quat(i,:) = rot2quat(Rreg(:,:,i))';
+        pos(i,:) = (i-1).*[0.01,0.01,0.01];
+    end
+    
+    for i = 1:size(Rdata,3)
+        quatd(i,:) = rot2quat(Rdata(:,:,i))';
+    end
+    posd = pos(indices,:);
+
+    for i = 1:size(Rreal,3)
+        quatreal(i,:) = rot2quat(Rreal(:,:,i))';
+    end
+    posd = pos(indices,:);
+    
+    posPlot = pos;
+    quatPlot = quat;
+
+    
+    extraTime = 1;
+    samplePeriod = 1/100;
+    onesVector = ones(extraTime*(1/samplePeriod), 1);
+    posPlot = [posPlot; [posPlot(end, 1)*onesVector, posPlot(end, 2)*onesVector, posPlot(end, 3)*onesVector]];
+    quatPlot = [quatPlot; [quatPlot(end, 1)*onesVector, quatPlot(end, 2)*onesVector, quatPlot(end, 3)*onesVector, quatPlot(end, 4)*onesVector]];
+
+    % Create 6 DOF animation
+    SamplePlotFreq = 2;
+                
+    for i = 1:size(X0,3)
+        quat0(i,:) = rot2quat(X0(:,:,i))';
+    end
+    ts = linspace(0,1,size(Rreg,3));
+    t0 = ts;
+    
+    figure
+    subplot(2,2,1);
+    plot(1:N2, quat(:,1),'r-','LineWidth',2);
+    hold on;grid on;
+    line=plot(1:N2, quat0(:,1),'go','LineWidth',1);
+    plot(1:N2, quatreal(:,1),'b-','LineWidth',1);
+    alpha(line,0.1);
+    legend({'Regression','Ground Truth','Input'},'FontName','Arial','FontSize',15);
+    title('q0','FontName','Arial','FontSize',15);
+    xlabel('Frame');
+%     ylabel('q_0');
+    
+    subplot(2,2,2);
+    plot(1:N2, quat(:,2),'r-','LineWidth',2);
+    hold on;grid on;
+    plot(1:N2, quatreal(:,2),'b-','LineWidth',1);
+    plot(1:N2, quat0(:,2),'go','LineWidth',1);
+    title('q1','FontName','Arial','FontSize',15);
+    xlabel('Frame');
+%     ylabel('q_1');
+    
+    subplot(2,2,3);
+    plot(1:N2, quat(:,3),'r-','LineWidth',2);
+    hold on;grid on;
+    plot(1:N2, quatreal(:,3),'b-','LineWidth',1);
+    plot(1:N2, quat0(:,3),'go','LineWidth',1);
+    xlabel('Frame');
+    title('q2','FontName','Arial','FontSize',15);
+%     ylabel('q_2');
+    
+    subplot(2,2,4);
+    plot(1:N2, quat(:,4),'r-','LineWidth',2);
+    hold on;grid on;
+    plot(1:N2, quatreal(:,4),'b-','LineWidth',1);
+    plot(1:N2, quat0(:,4),'go','LineWidth',1);
+    xlabel('Frame');
+    title('q3','FontName','Arial','FontSize',15);
+%     ylabel('q_3');
+                
 end
 
 
